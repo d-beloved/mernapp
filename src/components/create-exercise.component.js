043e9dbdc7794d0bfedc62c0fd5676f2dfd82d3b
d-beloved <morayodeji@gmail.com>
2019-09-05 22:42:21 +0100

    add components to the app

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: []
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+
+    console.log(exercise);
+
+    window.location = "/";
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Exercise component!</p>
+      </div>
+    );
+  }
+}
